ce982c90ba80d9f3f7c0a042882c8447bacd98c6
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-05 13:41:13 +0530

    fix: hide header and sidebar for auth view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import SideBar from '@/components/SideBar.vue';
 import Header from '@/components/Header.vue';
-
+const route = useRoute();
 </script>
 
 <template>
-  <Header/>
-  <SideBar/>
+  <Header v-if="!route.meta.hideHeaderAndSidebar" />
+  <SideBar v-if="!route.meta.hideHeaderAndSidebar"/>
   <RouterView/>
 </template>
