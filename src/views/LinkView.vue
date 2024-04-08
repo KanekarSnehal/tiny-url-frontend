8825ca25c4a44c275a5eb51280c5902416624a98
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-09 00:39:35 +0530

    fix: remove code for delete modal

--- src/views/LinkView.vue
+++ src/views/LinkView.vue
@@ -14,7 +14,7 @@
 
             <div class="flex flex-col gap-4">
               <div class="flex flex-col">
-                <p class="text-xl font-bold">{{ tinyUrl.title }}</p>
+                <p @click="navigateToDetails(tinyUrl.id)" class="text-xl font-bold">{{ tinyUrl.title }}</p>
                 <a target="_blank" :href="getTinyUrl(tinyUrl.custom_back_half ? tinyUrl.custom_back_half : tinyUrl.id)" class="font-bold text-sky-600">{{ tinyUrl.custom_back_half ? tinyUrl.custom_back_half : tinyUrl.id }}</a>
                 <a target="_blank" :href="tinyUrl.long_url" class="text-sm">{{ tinyUrl.long_url }}</a>
               </div>
@@ -38,7 +38,7 @@
             <button type="button" @click="openDeleteModal(tinyUrl.id)" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
               <IconDelete/>
             </button>
-            <button type="button"
+            <button @click="navigateToDetails(tinyUrl.id)" type="button"
               class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
               View Details
             </button>
@@ -54,28 +54,7 @@
      </div>
   </div>
 
-  <div v-if="isDeleteModalOpen" class="overflow-y-auto overflow-x-hidden fixed border-8 top-50 right-50 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" @click="closeModal" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this url?</h3>
-                <button type="button" @click="deleteUrl" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                    Yes, I'm sure
-                </button>
-                <button @click="closeModal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
-            </div>
-        </div>
-    </div>
-  </div>
+<DeleteUrlModal v-if="isDeleteModalOpen" @closeDeleteModal="closeModal" @submitDeleteDetails="deleteUrl"/>
 
 <EditUrlDetailsModal v-if="isEditModalOpen" :editUrlDetails="editUrlDetails" @closeEditModal="closeEditModal" @submitEditDetails="submitEditDetails"/>
 
@@ -85,14 +64,15 @@ import IconCalendar from '@/components/icons/IconCalendar.vue';
 import IconCopy from '@/components/icons/IconCopy.vue';
 import IconEdit from '@/components/icons/IconEdit.vue';
 import IconDelete from '@/components/icons/IconDelete.vue';
-import { useTinyUrlStore, type tinyUrlListDto } from '../stores/tinyUrl';
-import { onMounted, reactive, ref, computed } from 'vue';
+import { useTinyUrlStore, type tinyUrlDto } from '../stores/tinyUrl';
+import { onMounted, reactive, ref } from 'vue';
 import EditUrlDetailsModal from '@/components/EditUrlDetailsModal.vue';
-import { useForm } from 'vee-validate';
+import { useRouter } from 'vue-router';
+import DeleteUrlModal from '@/components/DeleteUrlModal.vue';
 
 const tinyUrlStore = useTinyUrlStore();
 
-const state: { tinyUrlList: tinyUrlListDto[] } = reactive({ tinyUrlList: [] });
+const state: { tinyUrlList: tinyUrlDto[] } = reactive({ tinyUrlList: [] });
 
 const isDeleteModalOpen = ref(false);
 
@@ -100,17 +80,17 @@ const deleteId = ref('');
 
 const isEditModalOpen = ref(false);
 
-const editUrlDetails = ref<Partial<tinyUrlListDto>>({
+const editUrlDetails = ref<Partial<tinyUrlDto>>({
   title: '',
   custom_back_half: '',
   id: '',
 });
 
-
+const router = useRouter();
 
 onMounted(async () => {
   const response = await tinyUrlStore.getAllTinyUrlList();
-  state.tinyUrlList = response.data.map((d: tinyUrlListDto) => ({ ...d, copied: false }));
+  state.tinyUrlList = response.data.map((d: tinyUrlDto) => ({ ...d, copied: false }));
 })
 
 function formattedDate(dateObject: { toLocaleDateString: (arg0: string, arg1: { year: string; month: string; day: string; }) => void; }) {
@@ -161,11 +141,11 @@ async function deleteUrl() {
   const deleteResponse = await tinyUrlStore.deleteTinyUrl(deleteId.value);
   if (deleteResponse.status == "success") {
       const response = await tinyUrlStore.getAllTinyUrlList();
-      state.tinyUrlList = response.data.map((d: tinyUrlListDto) => ({ ...d, copied: false }));
+      state.tinyUrlList = response.data.map((d: tinyUrlDto) => ({ ...d, copied: false }));
   }
 }
 
-function openEditModal(url: tinyUrlListDto) {
+function openEditModal(url: tinyUrlDto) {
   editUrlDetails.value = url;
   isEditModalOpen.value = true;
 }
@@ -174,12 +154,17 @@ function closeEditModal() {
   isEditModalOpen.value = false;
 };
 
-async function submitEditDetails(url: Partial<tinyUrlListDto>) {
+async function submitEditDetails(url: Partial<tinyUrlDto>) {
   const response = await tinyUrlStore.updateTinyUrl(url);
   if (response.status == "success") {
     isEditModalOpen.value = false;
     const response = await tinyUrlStore.getAllTinyUrlList();
-    state.tinyUrlList = response.data.map((d: tinyUrlListDto) => ({ ...d, copied: false }));
+    state.tinyUrlList = response.data.map((d: tinyUrlDto) => ({ ...d, copied: false }));
   }
 }
+
+function navigateToDetails(id: string) {
+  router.push(`/links/${id}/details`);
+}
+
 </script>
