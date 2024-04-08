0a5cf26b2fa6da203728e160170be50095d789cb
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-09 00:35:53 +0530

    feat: add arrow icon svg

---
+++ src/components/icons/IconLeftArrow.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" role="graphics-document" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <title>Icon</title>
+        <path fill="none" d="M0 0h24v24H0z"></path><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path>
+    </svg>
+</template>
