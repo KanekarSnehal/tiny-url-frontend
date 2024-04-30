df80efa55b56889409b15ef0aadb6de526aed8cd
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-30 23:37:38 +0530

    feat: add login with test creds

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,6 +9,7 @@
               name="email"
               type="email"
               v-model="email"
+              v-bind="emailProps"
               placeholder="Email Address"
               class="w-full px-4 py-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
             />
@@ -20,11 +21,13 @@
               name="password"
               type="password"
               v-model="password"
+              v-bind="passwordProps"
               placeholder="Password"
               class="w-full px-4 py-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
             />
             <p class="error text-red-600">{{ errors.password }}</p>
           </div>
+          <p class="border-b-2 border-primary-300  my-4 px-4 pt-2  hover:border-b-4 cursor-pointer font-semibold text-indigo-900 text-lg hover:text-indigo-800 self-center text-center" @click="fillTestCreditionals">Fill test creditionals</p>
           <div class="form-fields">
             <button
               class="w-full px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-indigo-200"
@@ -50,67 +53,72 @@
   
   
   <script setup lang="ts">
-//   import SocialLogin from '../components/SocialLogin.vue';
+  //   import SocialLogin from '../components/SocialLogin.vue';
   import { useRouter } from 'vue-router';
   import { useAuthStore } from '../stores/auth';
   import { ref } from 'vue';
-import { useForm } from 'vee-validate';
-  
+  import { useForm } from 'vee-validate';
+
   const router = useRouter();
   const userStore = useAuthStore();
   const error = ref('');
 
   // create form
-const { defineField, handleSubmit, errors } = useForm({
-  initialValues: {
-    email: '',
-    password: '',
-  },
-  validationSchema: {
-    email: validateField('email'),
-    password: validateField('password'),
-  },
-});
+  const { defineField, handleSubmit, errors } = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: {
+      email: validateField('email'),
+      password: validateField('password'),
+    },
+  });
 
-function validateField(field: string) {
-  switch (field) {
-    case 'email':
-      return (value: string) => {
-        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if (value && emailPattern.test(value)) {
-          return true;
-        }
-        return 'This field must be a valid email address';
-      };
-    case 'password':
-      return (value: string) => {
-        if (value && value.length >= 6) {
-          return true;
-        }
-        return 'This field must be at least 6 characters long';
-      };
-    default:
-      return true;
+  function validateField(field: string) {
+    switch (field) {
+      case 'email':
+        return (value: string) => {
+          const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          if (value && emailPattern.test(value)) {
+            return true;
+          }
+          return 'This field must be a valid email address';
+        };
+      case 'password':
+        return (value: string) => {
+          if (value && value.length >= 6) {
+            return true;
+          }
+          return 'This field must be at least 6 characters long';
+        };
+      default:
+        return true;
+    }
   }
-}
 
-// define fields
-const [email, emailProps] = defineField('email');
-const [password, passwordProps] = defineField('password');
-  
+  // define fields
+  const [email, emailProps] = defineField('email');
+  const [password, passwordProps] = defineField('password');
+
   const onSubmit = handleSubmit(async () => {
     try {
       await userStore.login({ email: email.value, password: password.value });
-      if(userStore.error.explanation) {
+      if (userStore.error.explanation) {
         error.value = userStore.error.explanation;
         return;
       }
       router.push('/home');
       // userStore.user.role == 1 ? router.push('/admin') : router.push('/home');
-      
+
     } catch (error) {
       // Handle error, such as displaying an error message
       console.error(error);
     }
   });
-  </script>
+
+  const fillTestCreditionals = () => {
+    email.value = import.meta.env.VITE_TEST_EMAIL
+    password.value = import.meta.env.VITE_TEST_PASSWORD
+  }
+</script>
