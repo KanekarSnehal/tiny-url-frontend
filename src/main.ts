564cfa6bf5d25720c92179c17c16f51c8383eb14
KanekarSnehal <snehalkanekar43@gmail.com>
2024-05-01 15:54:26 +0530

    feat: add toast import in app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import  { HttpClient } from './interceptors/http-interceptor';
+import { HttpClient } from './interceptors/http-interceptor';
 import { createPinia } from 'pinia';
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 import './input.css';
 const store = createPinia();
+import "vue3-toastify/dist/index.css";
 
 const app = createApp(App);
 
@@ -14,6 +16,10 @@ HttpClient.install(app);
 // Use router and store
 app.use(router);
 app.use(store);
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+    clearOnUrlChange: false,
+} as ToastContainerOptions);
 
 // Mount the app
 app.mount('#app');
